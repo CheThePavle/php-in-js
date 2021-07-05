7da082600f579fbddd5b4f089dbae11dd9acfeb4
Chikurov Pavel <84593835+CheThePavle@users.noreply.github.com>
2021-07-05 21:45:36 +0400

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,24 +1,27 @@
 <?php
-<!DOCTYPE html>
-<html lang="en">
+echo 
+'
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Vercel + PHP</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.1.4/tailwind.min.css" />
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Vercel + PHP</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.1.4/tailwind.min.css" />
+    </head>
 
-<body class="font-mono bg-gray-300 w-screen h-screen flex items-center justify-center">
-    <div>
-        <div class="text-5xl font-bold">
-            Vercel + PHP
-            <a class="inline-block" href="https://vercel.com/new/project?template=https://github.com/juicyfx/vercel-examples/tree/master/php">
-                <img src="https://vercel.com/button" alt="Deploy to Vercel" />
-            </a>
+    <body class="font-mono bg-gray-300 w-screen h-screen flex items-center justify-center">
+        <div>
+            <div class="text-5xl font-bold">
+                Vercel + PHP
+                <a class="inline-block" href="https://vercel.com/new/project?template=https://github.com/juicyfx/vercel-examples/tree/master/php">
+                    <img src="https://vercel.com/button" alt="Deploy to Vercel" />
+                </a>
+            </div>
         </div>
-    </div>
-</body>
+    </body>
 
-</html>
+    </html>
+'
